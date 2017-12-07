e52ffe0ffcbe121953242d2666b6e1a0fcf338a5
nghiepuit <nghiepuit@gmail.com>
2017-12-07 23:58:43 +0700

    update using es6 class arrowfunction ...

--- src/index.js
+++ src/index.js
@@ -4,16 +4,9 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import moment from 'moment'
-import appReducers from './reducers/index';
 
+import appReducers from './reducers/index';
 import App from './components/App/App';
-import appReducers from './reducers/index';
-
-import 'font-awesome/css/font-awesome.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'jquery';
-import 'bootstrap/dist/js/bootstrap.js';
-import './../views/fonts/Roboto.css';
 
 // thiết lập cho global
 moment.locale('en')
@@ -21,14 +14,6 @@ import 'font-awesome/css/font-awesome.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'jquery';
 import 'bootstrap/dist/js/bootstrap.js';
-
-import App from '@components/App';
-
-const store = createStore(
-    appReducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk)
-);
 
 const store = createStore(
     appReducers,
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,7 @@
 import { combineReducers } from 'redux';
-<<<<<<< HEAD
 
 const appReducers = combineReducers({
 
-=======
-import { paginate } from './course';
-
-const appReducers = combineReducers({
-    paginate
->>>>>>> feed75a996568a085715a1c26bd607029f051f97
 });
 
 export default appReducers;
